c25cfefa81ba9cdd24adcb1b2fe480d310e62f64
DemyanSafronov <45840508+DemyanSafronov@users.noreply.github.com>
2019-03-06 22:13:34 +0300

    Add files via upload

---
+++ script.php
@@ -0,0 +1,4 @@
+<?php 
+echo "Access to XMLHttpRequest at 'bla bla bla' from origin 'null' has been blocked by CORS policy: Cross origin requests are only supported for protocol schemes: http, data, chrome, chrome-extension, https. ";
+echo 'Т.е. index79.html и script.php должны вызываться по http, чтобы все сработало нормально.';
+?>
